d7c50da6f92b82ab6b919da2dfe8182d5de47895
mrsidnaik <mrsidnaik@gmail.com>
2019-12-26 04:09:56 -0800

    Add files via upload

--- assignment/Organization.java
+++ assignment/Organization.java
@@ -2,6 +2,16 @@ package assignment;
 
 public class Organization {
 
+	static Employee[] emps = new Employee[5];
+	static int limit =5;
+	int counter =5;
+	
+	//public void addEmp(Employee e)
+	//{
+	//	if (counter< limit)
+	//		emps[counter++]=e;
+	//}
+	
 	public static void main(String[] args) {
 		
 		Department HR = new Department("HR","Hyderabad");
@@ -17,9 +27,70 @@ public class Organization {
 		Address add4 = new Address(314, "Mindspace", "Hyderabad", "Telengana");
 		Employee e4 = new Employee("Roh",add4,1,TS);
 		Address add5 = new Address(315, "Mindspace", "Hyderabad", "Telengana");
-		Employee e6 = new Employee("Sach",add5,1);
+		Employee e5 = new Employee("Sach",add5,1, null);
+		
+		
+		//org.addEmp(e1);
+		//org.addEmp(e2);
+		//org.addEmp(e3);
+		//org.addEmp(e4);
+		//org.addEmp(e5);
+		emps[0]=e1;
+		emps[1]=e2;
+		emps[2]=e3;
+		emps[3]=e4;
+		emps[4]=e5;
+		
+		deptName(e1);
+		deptList("HR");
+		deptNull();
+		
+		
 		
 		
 	}
 
-}
+	public static void deptName(Employee e)
+	{
+		System.out.println(e.getDept().getName());
+	}
+	
+	public static void deptList(String dept)
+	{
+		for(int i = 0; i<5; i++)
+		{
+			Department d = emps[i].getDept();
+			if(d != null)
+			{
+				String deptName = d.getName();
+				if(deptName.equalsIgnoreCase(dept)) {
+					System.out.println(emps[i]);
+				}
+			}
+		}
+	}
+	
+	public static void getDept(String name) {
+		for (Employee e : emps) {
+			if(e.getName().equalsIgnoreCase(name)) {
+				System.out.println(e.getDept().getName());
+		
+			}
+		}
+	}
+	
+	
+	public static void deptNull()
+	{
+		for(int i = 0; i<5; i++)
+		{
+			Department d = emps[i].getDept();
+			if(d == null)
+			{
+					System.out.println(emps[i].getName());
+				}
+			}
+		}
+	}
+	
+
