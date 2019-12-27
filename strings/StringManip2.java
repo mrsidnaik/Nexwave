675308ec042c8c88f34358fb45be4e158099c7e3
mrsidnaik <mrsidnaik@gmail.com>
2019-12-27 18:04:27 +0530

    Add files via upload

--- strings/StringManip2.java
+++ strings/StringManip2.java
@@ -3,8 +3,50 @@ package strings;
 public class StringManip2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("<html>");
+		buffer.append("<head><title>My Web Page</title></head>");
+		buffer.append("<body>");
+		buffer.append("<h1>Thsi is my web profile<>/h1");
+		buffer.append("</body></html>");
+		
+		System.out.println(buffer.toString());
+		
+		String s ="Namanx";
+		System.out.println(checkPalin(s));
+		
+		System.out.println(checkAll("Hellodhjklsx"));
 
+	}
+	
+	public static boolean checkPalin(String s)
+	{
+		StringBuffer str = new StringBuffer(s);
+		return(str.equals(str.reverse()));
+				
+	}
+	
+	public static int checkAll(String str)
+	{
+		int len = str.length();
+		String s= str.toLowerCase();
+		int ctr=0;
+		char[] ch= new char[26];
+		for(char i='a';i<='z';++i)
+		{
+			for(int j=0;j<len;++j)
+			{
+				if(i==s.charAt(j))
+				{
+					for(int k = 0; k<26;++k)
+					{
+						if(i==ch[k])
+							break;
+					}
+				}
+			}
+		}
+		return ctr;
 	}
 
 }
