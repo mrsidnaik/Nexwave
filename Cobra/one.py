b9f188b301640edd6e2d9db2e11a6ec1824a0f32
mrsidnaik <mrsidnaik@gmail.com>
2020-01-08 17:27:00 +0530

    Add files via upload

---
+++ Cobra/one.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def red():
+    print("blood")
+
+
+# In[2]:
+
+
+def yellow():
+    print("lemon")
+
+
+# In[3]:
+
+
+def orange():
+    red()
+    yellow()
+    print("the above makes orange")
+
+
+# In[ ]:
+
+
+def blue():
+    print("wednesday blues")
+
